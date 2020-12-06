6fded032c4054f897033d6fd70509964f826b644
Simon Tartakovksy <simtart@gmail.com>
2020-12-06 17:00:45 -0500

    I hate myself

--- day6.py
+++ day6.py
@@ -19,7 +19,7 @@ for line in sl:
     vals.append(line.strip().split())
 
 print(sum(num_good))
-
+print(len(num_good))
 num_good = []
 
 vals = []
@@ -36,8 +36,15 @@ for line in sl:
 
 print(sum(num_good))
 
+from collections import Counter
 
 #
 print("part 1:", sum([ len([1 for letter in alpha if letter in reduce(lambda x,y: x.strip() + y.strip(), sl[x[0] :  x[1]])]) for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])]))
 
-print("part 2:", sum([ len([1 for letter in alpha if reduce(lambda x,y: x.strip() + y.strip(), sl[x[0] :  x[1]]).count(letter) == len([sl[x[0] :  x[1]] for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])])]) for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])]))
+
+count_list =list((list(filter(lambda z: False if not z else True, list(map(lambda x: dict(Counter(x.strip())), x)))) for x in [sl[x[0] :  x[1]] for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])]))[:-1]
+#print(reduce(lambda a,b:[lette for lette in a if lette in b.keys() and lette in a], count_list[1], list(alpha)))
+good = sum([len(reduce(lambda a,b:[lette for lette in a if lette in b.keys() and lette in a], group_ans, list(alpha))) for group_ans in count_list])
+print("part2:", sum([len(reduce(lambda a,b:[lette for lette in a if lette in b.keys() and lette in a], group_ans, list(alpha))) for group_ans in list((list(filter(lambda z: False if not z else True, list(map(lambda x: dict(Counter(x.strip())), x)))) for x in [sl[x[0] :  x[1]] for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])]))[:-1]]))
+#print("part 2:", list(filter(lambda z: True if not z[0] else False, (list(map(lambda x: dict(Counter(x.strip())), x)) for x in [sl[x[0] :  x[1]] for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])]))))
+#print("part 2:", sum([ len([1 for letter in alpha if reduce(lambda x,y: x.strip() + y.strip(), sl[x[0] :  x[1]]).count(letter) == len([sl[x[0] :  x[1]] for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])])]) for x in zip([0] + [ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0],[ind for ind in range(len(sl)) if len(sl[ind].strip()) == 0] + [len(sl)])]))
